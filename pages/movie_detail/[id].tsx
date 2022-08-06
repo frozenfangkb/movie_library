a56a91c544c0b3b47e24de3ac9d7161f0c0c8471
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-08-06 14:32:01 +0200

    Base logic for movie detail component

--- pages/movie_detail/[id].tsx
+++ pages/movie_detail/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../../store/hooks";
+import { selectMovies } from "../../store/slices/movieSlice";
+import { Movie } from "../../models/movie";
 
 const MovieDetail: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
+  const movies = useAppSelector(selectMovies);
+  const [selectedMovie, setSelectedMovie] = useState<Movie>({} as Movie);
+
+  useEffect(() => {
+    if (id && id !== "" && !isNaN(+id)) {
+      const movie = movies.find((x) => x.id === +id);
+      if (movie) {
+        setSelectedMovie(movie);
+      } else {
+        // TODO: Call API and retrieve movie by id
+      }
+    }
+  }, [id, movies]);
 
   return <div>ID is {id}</div>;
 };
